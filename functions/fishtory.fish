393dad5dece51dd8f91b9693200f956e9cd54513
enricobacis <enrico.bacis@gmail.com>
2016-04-04 18:53:16 +0200

    first commit

---
+++ functions/fishtory.fish
@@ -0,0 +1,27 @@
+# SYNOPSIS
+#   !! insert last command
+#   !$ insert last command's arguments
+
+function bind_bang
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+function fishtory -d "fishtory"
+  bind ! bind_bang
+  bind '$' bind_dollar
+end
